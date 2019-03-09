d835ee943576c8266d58533e591020619e81688e
Dor Tzur <dortzur@gmail.com>
2019-03-09 23:17:42 +0200

    remove stuff

--- services/log.go
+++ services/log.go
@@ -1,17 +1,12 @@
 package services
 
 import (
-	"fmt"
 	"go.uber.org/zap"
 )
 
 func initLogger() *zap.SugaredLogger {
-	y := 42
-	fmt.Println(y)
-
 	logger, _ := zap.NewProduction()
 	sugar := logger.Sugar()
-	sugar.Info("hello")
 	return sugar
 }
 
